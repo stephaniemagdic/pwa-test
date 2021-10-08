5b4fd7cd078f650cf647bdd55f1918d377b5695f
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-10-08 11:24:59 -0600

    Add notifications file for later use

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+// import setupNotifications from './setupNotifications';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -21,3 +22,5 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// setupNotifications();
